65896f9109b866c0f8a8c6cea53276646c28fa90
Gitarackur <peterosah5@gmail.com>
2024-08-11 12:43:28 +0100

    Admin pages

---
+++ pages/admin/dashboard/campaign/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col gap-11 md:grid md:grid-cols-3">
+        <AdminStatusCard />
+        <AdminStatusCard />
+        <AdminStatusCard />
+    </div>
+</template>
+
+<script setup lang="ts">
+// definePageMeta({
+//     name: "Admin Dashboard",
+//     middleware: [async () => { }],
+// });
+</script>
+
+<style></style>
