d5b296c6118aff4cf3674fa26fa1650de2c7a7df
Gitarackur <peterosah5@gmail.com>
2024-08-11 16:24:46 +0100

    admin dashboard

--- components/Admin/Dashboard/Navigator/index.vue
+++ components/Admin/Dashboard/Navigator/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-col gap-[1rem]">
-        <AdminDashboardNavigatorMenuItem label="Explore" icon="nav/search" to="Explore" />
-        <AdminDashboardNavigatorMenuItem label="Campaigns" icon="nav/campaign" to="Campaign" />
-        <AdminDashboardNavigatorMenuItem label="Influencers" icon="nav/influencers" to="/dashboard/influencers" />
-        <AdminDashboardNavigatorMenuItem to="Brands" label="Brands" icon="nav/brands" />
-        <AdminDashboardNavigatorMenuItem label="Finance" icon="nav/payment" to="Finance" />
+        <AdminDashboardNavigatorMenuItem label="Dashboard" icon="nav/search" to="/admin/dashboard" />
+        <AdminDashboardNavigatorMenuItem label="Campaigns" icon="nav/campaign" to="/admin/dashboard/campaign" />
+        <AdminDashboardNavigatorMenuItem label="Influencers" icon="nav/influencers" to="/admin/dashboard" />
+        <AdminDashboardNavigatorMenuItem label="Brands" icon="nav/brands" to="/admin/dashboard" />
+        <AdminDashboardNavigatorMenuItem label="Finance" icon="nav/payment" to="/admin/dashboard" />
     </div>
 </template>
 
