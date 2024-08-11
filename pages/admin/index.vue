d5a4c969cd8f918cfc5f0e10ff20cd53a34f1a9c
Treasure <treasure.o@tima.agency>
2024-08-11 13:31:49 +0100

    updating login

---
+++ pages/admin/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  name: "AdminRedirect",
+  middleware: [
+    async () => {
+      return navigateTo("/admin/dashboard");
+    },
+  ],
+});
+</script>
+
+<style></style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -12,35 +12,3 @@
     </NuxtLayout>
   </div>
 </template>
-
-<script setup lang="ts">
-definePageMeta({
-  middleware: [
-    "options",
-    (to, from) => {
-      const authStore = useAuthStore();
-      const userType = authStore.authorization.userType;
-
-      if (!Object.keys(to.params).includes("type")) {
-        return navigateTo(
-          userType === constants.INFLUENCER
-            ? {
-                name: "Explore",
-                params: {
-                  type: userType,
-                },
-              }
-            : {
-                name: "BrandCampaign",
-                params: {
-                  type: userType,
-                },
-              }
-        );
-      }
-    },
-  ],
-});
-</script>
-
-<style></style>
