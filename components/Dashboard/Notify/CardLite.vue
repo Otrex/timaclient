6da62817c8b04c6eb2e4dc339ceaa7c0020e84c0
Gitarackur <peterosah5@gmail.com>
2024-10-07 14:57:18 +0200

    UI WIP

--- components/Dashboard/Notify/CardLite.vue
+++ components/Dashboard/Notify/CardLite.vue
@@ -1,41 +1,54 @@
 <template>
   <div class="mb-4" :style="style">
-    <h3 class="text-base font-semibold">{{ title }}</h3>
-    <p class="text-sm text-gray-500">{{ message }}</p>
-    <div class="flex items-center text-sm text-gray-400 mt-2">
-      <svg
-        class="w-4 h-4 mr-1"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M8 7V3M16 7V3M5 11h14M5 19h14M7 23h10a2 2 0 002-2v-6a2 2 0 00-2-2H7a2 2 0 00-2 2v6a2 2 0 002 2z"
-        ></path>
-      </svg>
-      <span>{{ time }}</span>
-    </div>
-    <div>
-      <span class="flex items-center">
-        <span class="w-2 h-2 rounded-full mr-1 bg-[--status]">&nbsp;</span>
-        <span class="text-[--status] text-sm">{{ status }}</span>
-      </span>
+    <div class="flex justify-between">
+      <div>
+        <h3 class="text-base font-semibold">{{ title }}</h3>
+        <p class="text-sm text-gray-500">{{ message }}</p>
+        <div class="flex items-center text-sm text-gray-400 mt-2">
+          <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M8 7V3M16 7V3M5 11h14M5 19h14M7 23h10a2 2 0 002-2v-6a2 2 0 00-2-2H7a2 2 0 00-2 2v6a2 2 0 002 2z">
+            </path>
+          </svg>
+          <span>{{ time }}</span>
+        </div>
+        <div>
+          <span class="flex items-center">
+            <span class="w-2 h-2 rounded-full mr-1 bg-[--status]">&nbsp;</span>
+            <span class="text-[--status] text-sm">{{ status }}</span>
+          </span>
+        </div>
+      </div>
+
+      <div>
+        <NuxtLink :to="`/dashboard/${route.params.type}/campaigns/applications/${2}/schedule-post`"
+          v-if="props.status === 'Accepted'"
+          class="text-[#545454] text-base leading-[18.75px] border border-[#BBBBBB] rounded-lg p-4">
+          Schedule Post
+        </NuxtLink>
+        <NuxtLink :to="`/dashboard/${route.params.type}/campaigns/applications/${2}/content-revision`"
+          v-if="props.status === 'Rejected'"
+          class="text-[#545454] text-base leading-[18.75px] border border-[#BBBBBB] rounded-lg p-4">
+          Review</NuxtLink>
+        <NuxtLink :to="`/dashboard/${route.params.type}/campaigns/applications/${2}/content-revision`"
+          v-if="props.status === 'Draft'"
+          class="text-[#545454] text-base leading-[18.75px] border border-[#BBBBBB] rounded-lg p-4">Schedule
+          Submit Content</NuxtLink>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    title: String,
-    message: String,
-    time: String,
-    status: String,
-    style: String,
-  },
-};
+<script lang="ts" setup>
+const props = defineProps({
+  title: String,
+  message: String,
+  time: String,
+  status: String,
+  style: String,
+});
+
+const route = useRoute();
+
 </script>
---
+++ pages/dashboard/[type]/campaigns/applications/[id]/schedule-post.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="px-4">
+        <UiImg src="/assets/svg/content-revision-thumbnail.jpeg" alt="content-revision" class="w-full h-[200px]" />
+
+        <div class="mt-8">
+            <h1 class="text-2xl font-semibold leading-[28.13px] text-[#333333] mb-2">Schedule Post</h1>
+            <p class="text-base font-semibold leading-[18.75px] text-[#333333] mb-6">Choose when your post will be
+                shared
+            </p>
+            <div class="border border-[#BBBBBB]"></div>
+        </div>
+
+
+        <div class="flex mt-12">
+            <div class="md:w-5/12">
+                <Calendar />
+            </div>
+
+            <div class="md:w-7/12">
+                <div class="mb-6">
+                    <h1 class="text-2xl font-semibold leading-[28.13px] text-[#333333] mb-2">Time</h1>
+                    <DatePicker mode="time" />
+                </div>
+
+                <textarea
+                    class="text-lg leading-[21.09px] outline-none shadow-none w-5/12 h-auto rounded-lg border-[2px] border-[#2DA2FD] placeholder:text-[##B0B0B0]"
+                    resize="none" value="Time zone: (GMT +1:00) Lagos/Nigeria"></textarea>
+            </div>
+        </div>
+
+        <div class="mt-12 flex items-center justify-center">
+            <UiButtonDefault @click="() => submitContentRevision()" variant="primary" label="Apply"
+                class="min-w-[21.875rem] p-2" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    name: "Accepted Content",
+});
+import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { Calendar, DatePicker } from "v-calendar";
+import "v-calendar/style.css";
+// const emits = defineEmits(["update:modelValue"]);
+
+const props = defineProps({
+    modelValue: {
+        type: [Date, Array, Object],
+        default: () => new Date(),
+    },
+});
+
+
+const route = useRoute();
+const router = useRouter();
+
+const submitContentRevision = () => {
+    console.log('texg');
+    router.replace('attach-files');
+};
+
+const range = computed({
+    get() {
+        return new Date();
+    },
+    set(value) {
+        // emits("update:modelValue", value);
+    },
+});
+</script>
+
+<style></style>
