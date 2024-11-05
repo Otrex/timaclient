05af9123ffe6d393f3fcf402ef14bfaf87e8f151
Treasure <treasure.o@tima.agency>
2024-11-05 07:37:38 +0100

    order-calculation-fix

--- lib/api/types/responses.ts
+++ lib/api/types/responses.ts
@@ -46,3 +46,30 @@ export interface YoutubeChannelResponse {
   channel_url: string;
 }
 
+export interface TiktokProfileResponse {
+  user_id: string;
+  unique_id: string;
+  nickname: string;
+  signature: string;
+  avatar_url: string;
+  follower_count: number;
+  following_count: number;
+  total_videos: number;
+  total_hearts: number;
+  verified: boolean;
+  engagement_metrics: {
+    total_likes: number;
+    total_comments: number;
+    total_views: number;
+    total_shares: number;
+    average_likes_per_video: number;
+    average_comments_per_video: number;
+    average_views_per_video: number;
+    average_shares_per_video: number;
+    engagement_rate: number;
+    estimated_reach: number;
+    estimated_impressions: number;
+  };
+}
+
+
